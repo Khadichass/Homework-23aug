8c3ab43f05583facd1a7d483bc15c3da73db3998
Khadicha <246abdurashidova123@gmail.com>
2023-08-25 12:58:48 +0500

    ex12 done

--- ex12.c
+++ ex12.c
@@ -2,6 +2,25 @@
 
 int main(){
 
+  for (int i = 1; i <= 100; i++)
+  {
+    if (i % 3 == 0 && i % 5 == 0)
+    {
+      printf("FizzBuzz\n");
+    }
+    if (i % 3 == 0 )
+    {
+      printf("Fizz\n");
+    }
+    if (i % 5 == 0 )
+    {
+      printf("Buzz\n");
+    }
+    else{
+      printf("%d",i);
+    }
+    
+  }
   
 
   return 0;
